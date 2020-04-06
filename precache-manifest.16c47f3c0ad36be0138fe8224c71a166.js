b9fe26418c0d19b3498a1068fe322388178b7294
taha <taha.rehah@gmail.com>
2020-04-06 22:02:22 +0100

    Updates

---
+++ precache-manifest.16c47f3c0ad36be0138fe8224c71a166.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912d68085ba0cb3b0e123e25cc053d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8388ca021c48fee8dc6",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "682de7b629a076afd057",
+    "url": "/static/css/main.ed84b456.chunk.css"
+  },
+  {
+    "revision": "e8388ca021c48fee8dc6",
+    "url": "/static/js/2.abd4c22c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.abd4c22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682de7b629a076afd057",
+    "url": "/static/js/main.8af27e10.chunk.js"
+  },
+  {
+    "revision": "7074af389f7d8556acf7",
+    "url": "/static/js/runtime-main.95a8dce5.js"
+  }
+]);
